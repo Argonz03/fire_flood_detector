9211b7b5e5588df51c0f79718d16d4db2baca2c6
Raul Gonzalez <rgonzalez0691@gmail.com>
2024-10-22 23:16:41 -0400

    Moved loRa code to main file

--- fire_flood_detection.ino
+++ fire_flood_detection.ino
@@ -1,7 +1,7 @@
 #include <Wire.h>
 #include "DigitalSensors.h"
 #include "ADCManager.h" 
-#include "LoraTX.h"
+#include "LoRaWan_APP.h"
 #include <ArduinoJson.h>
 
 //define pin data
@@ -10,9 +10,37 @@
 #define pinSCL 33
 #define pinWater 46
 
-#define BUFFER_SIZE 256
+#define RF_FREQUENCY 915000000 // Hz
+
+#define TX_OUTPUT_POWER 5        // dBm
+
+#define LORA_BANDWIDTH 0         // [0: 125 kHz,
+                                                              //  1: 250 kHz,
+                                                              //  2: 500 kHz,
+                                                              //  3: Reserved]
+#define LORA_SPREADING_FACTOR 7         // [SF7..SF12]
+#define LORA_CODINGRATE 1         // [1: 4/5,
+                                                              //  2: 4/6,
+                                                              //  3: 4/7,
+                                                              //  4: 4/8]
+#define LORA_PREAMBLE_LENGTH 8         // Same for Tx and Rx
+#define LORA_SYMBOL_TIMEOUT 0         // Symbols
+#define LORA_FIX_LENGTH_PAYLOAD_ON false
+#define LORA_IQ_INVERSION_ON false
+
+
+#define RX_TIMEOUT_VALUE 1000
+#define BUFFER_SIZE 256 // Define the payload size here
 
 char jsonBuffer[BUFFER_SIZE];
+
+char txpacket[BUFFER_SIZE];
+
+bool lora_idle=true;
+
+static RadioEvents_t RadioEvents;
+void OnTxDone( void );
+void OnTxTimeout( void );
 
 ADCManager ADC;
 
@@ -21,8 +49,6 @@ IRTemp IRT;
 WaterDetector WD(pinWater);
 
 Temp_Hum TH(pinDHT22);
-
-LoraTX lora;
 
 StaticJsonDocument<BUFFER_SIZE> jsonDoc;
 
@@ -34,10 +60,21 @@ void setup() {
 
   Wire.begin(34, 33);
 
+  Mcu.begin(HELTEC_BOARD,SLOW_CLK_TPYE);
+
   ADC.begin();
 
   IRT.begin();
 
+  RadioEvents.TxDone = OnTxDone;
+  RadioEvents.TxTimeout = OnTxTimeout;
+    
+  Radio.Init( &RadioEvents );
+  Radio.SetChannel( RF_FREQUENCY );
+  Radio.SetTxConfig( MODEM_LORA, TX_OUTPUT_POWER, 0, LORA_BANDWIDTH,
+                     LORA_SPREADING_FACTOR, LORA_CODINGRATE,
+                     LORA_PREAMBLE_LENGTH, LORA_FIX_LENGTH_PAYLOAD_ON,
+                     true, 0, 0, LORA_IQ_INVERSION_ON, 3000 ); 
 
   
 }
@@ -73,14 +110,34 @@ void loop() {
 
   serializeJson(jsonDoc, jsonBuffer);
 
-  lora.loraTransmit(jsonBuffer);
+  if(lora_idle == true){
+    Radio.Send((uint8_t *)jsonBuffer, strlen(jsonBuffer));
+    lora_idle = false;
+
+  }
+
+
+
+
+  delay(2000); 
+
+  Radio.IrqProcess( );
+
+
 
   
+}
 
 
-  delay(2000); //Collecting period should be : >1.7 second
+void OnTxDone( void )
+{
+	Serial.println("TX done......");
+	lora_idle = true;
+}
 
-  lora.processRadioEvents();
-
-  
+void OnTxTimeout( void )
+{
+    Radio.Sleep( );
+    Serial.println("TX Timeout......");
+    lora_idle = true;
 }
